299acbb3945743918a3a8edb79ff7fa4a0ef482a
mdyasinahmed <yasinahmed7875@gmail.com>
2023-06-03 22:14:21 +0600

    switch case with cpp practice code is added

---
+++ switch-case.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int GPA;
+    cout<<"Enter Integer Part of Your GPA: ";
+    cin>>GPA;
+
+    switch(GPA)
+    {
+    case 5:
+        cout<<"Excellent";
+        break;
+    case 4:
+        cout<<"Good";
+        break;
+    case 3:
+        cout<<"Not Bad";
+        break;
+    case 2:
+        cout<<"Poor";
+        break;
+    default:
+        cout<<"Invalid Input";
+        break;
+    }
+
+
+    return 0;
+}
